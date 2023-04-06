3b87192e69e24242da76455c0fb7409f8d0213a0
Cicero Rikelme Lopes Batista <cicerorikelmelopesbatista@gmail.com>
2023-04-06 12:33:31 -0300

    Versão 1.9.2
    Adição da tabela  de edição do usuário Root e demais alterações no css,php e html.

--- UVS/pages/conta.php
+++ UVS/pages/conta.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-print_r($_SESSION);
+
 
 if(isset($_SESSION['email']) && isset($_SESSION['senha'])){
     $logado = $_SESSION['email'];
@@ -17,6 +17,27 @@ if(isset($_SESSION['email']) && isset($_SESSION['senha'])){
 }
 ?>
 
+<?php 
+    if(isset($_POST['mandar'])){
+     $arquivo = $_FILES['pegar'];
+     $arquivoNovo = explode(".",$arquivo['name']);
+     if($arquivoNovo[sizeof($arquivoNovo)-1]!='jpg'){
+         echo('Não é possivel enviar esse tipo de arquivo');
+     }else{
+        echo 'Vamos proseguir';
+        move_uploaded_file($arquivo['tmp_name'],'uploads/'.$arquivo['name']);
+     }
+    }
+  ?>
+
+  <?php 
+     include_once('../banco/dbconnect.php');
+
+     $sql = "SELECT * FROM tbubs";
+     $conn = $conexao ->query($sql) or die($conexao -> error);
+    
+  ?>
+
 <!-- Teste -->
 <!DOCTYPE html>
 <html>
@@ -32,6 +53,7 @@ if(isset($_SESSION['email']) && isset($_SESSION['senha'])){
     <title>UVS | Conta</title>
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
 
 
@@ -79,24 +101,41 @@ if(isset($_SESSION['email']) && isset($_SESSION['senha'])){
       }?></h1>
     <h3 class="txt2">Você está logado!</h3>
 </div>
+   
+<table class="tabela">
+        <tr>
+          <td>Email do Usuário</td>
+          <td>Senha do Usuário</td>
+          <td>Nome do Usuário</td>
+          <td>Data de Nascimento</td>
+          <td>id<td>
+        </tr>
+            <?php while($dado = $conn -> fetch_array()){ ?>
+        <tr>
+          <td><?php echo $dado["email"]; ?></td>
+          <td><?php echo $dado["senha"]; ?></td>
+          <td><?php echo $dado["nome"];  ?></td>
+          <td><?php echo $dado["data"];  ?></td>
+          <td><?php echo $dado["id"];  ?></td>
+          <td> 
+            <a class="btn btn-primary" type="submit" href="edit.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-brush-fill" viewBox="0 0 16 16">
+  <path d="M15.825.12a.5.5 0 0 1 .132.584c-1.53 3.43-4.743 8.17-7.095 10.64a6.067 6.067 0 0 1-2.373 1.534c-.018.227-.06.538-.16.868-.201.659-.667 1.479-1.708 1.74a8.118 8.118 0 0 1-3.078.132 3.659 3.659 0 0 1-.562-.135 1.382 1.382 0 0 1-.466-.247.714.714 0 0 1-.204-.288.622.622 0 0 1 .004-.443c.095-.245.316-.38.461-.452.394-.197.625-.453.867-.826.095-.144.184-.297.287-.472l.117-.198c.151-.255.326-.54.546-.848.528-.739 1.201-.925 1.746-.896.126.007.243.025.348.048.062-.172.142-.38.238-.608.261-.619.658-1.419 1.187-2.069 2.176-2.67 6.18-6.206 9.117-8.104a.5.5 0 0 1 .596.04z"/>
+</svg></a>
+            <a class="btn btn-danger" type="submit" href="edit.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+  <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
+</svg></a>
+          <td>
+        </tr>
+        <?php }  ?>
+</table>
+
+
 
 <form action="" method="POST" enctype="multipart/form-data"> 
    <label for="conteudo">Enviar imagem:</label>
    <input type="file" name="pegar" accept="image/*">    
    <button type="submit" name="mandar">Enviar imagem</button>
 </form>
-
-<?php 
-    if(isset($_POST['mandar'])){
-     $arquivo = $_FILES['pegar'];
-     $arquivoNovo = explode(".",$arquivo['name']);
-     if($arquivoNovo[sizeof($arquivoNovo)-1]!='jpg'){
-         echo('Não é possivel enviar esse tipo de arquivo');
-     }else{
-        echo 'Vamos proseguir';
-        move_uploaded_file($arquivo['tmp_name'],'uploads/'.$arquivo['name']);
-     }
-    }
-?>
+ 
   </body>
 </html>
---
+++ UVS/pages/edit.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/1ab94d0eba.js" crossorigin="anonymous"></script>
+    <link rel='icon' type='image/jpg' href="../imagens/iconsite1.jpg">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>UVS | Cadastro</title>
+    <link rel="stylesheet" href="../CSS/login.css">
+</head>
+<body>
+
+  <div class="login1">
+  <img id="imagem1" src="../IMAGENS/logo.png" width="70%" ></img>
+     <h3>Você é importante!<br>
+         Junte-se a nós!
+     <h3>
+     <p>Efetue o cadastro para acessar o site</p>
+   </div>
+    <main class="container">
+        <h2>UVS | Cadastro</h2>
+
+        <form name="form1" method="post" action="../banco/dbcadastro.php" onsubmit="return valida_form()">
+        <div class="input-field">
+                <label>Nome do Usuário:</label>
+                <input class="form-control" type="text" name="nome" id="nome"
+                    placeholder="insira o nome">
+                <label>Data de Nascimento:</label>
+                <input class="form-control" type="date" name="data" id="data" 
+                    placeholder="Insira a data">
+                <label>Email do Usuário:</label>
+                <input class="form-control" type="text" name="email" id="email"
+                    placeholder="insira o email">
+                <label>Senha do Usuário:</label>
+                <input class="form-control" type="password" name="senha" id="senha"
+                    placeholder="insira a senha">
+                <div class="underline"></div>
+            </div> 
+            </div>
+
+            <input type="submit" value="Alterar">
+
+
+        </form>
+
+        
+    </main>
+
+</body>
+</html>
