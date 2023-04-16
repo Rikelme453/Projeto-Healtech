171440614edc92d96d05e539284e8d2140d99d03
aureliointhernet2010 <aurelio2013silva@gmail.com>
2023-04-16 11:11:02 -0300

    versao 3.2

--- UVS/pages/quem_somos.php
+++ UVS/pages/quem_somos.php
@@ -4,9 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel=stylesheet href="../CSS/style.css">
     <title>Quem somos</title>
 </head>
 <body>
+
+    
+<nav class="menu3">
+    <div class="navbar">
+      <i class='bx bx-menu'></i>
+      <img class="img" src="../IMAGENS/site_logo.png" ></img>
+        <p id="text">Mais Vida! Mais Saúde!<p>
+       <div class="nav-links"> 
+       <ul class="links">
+          <li><a href="#">HOME</a></li>       
+          <li>
+            <a href="#">CONTA</a>
+            <i class='bx bxs-chevron-down htmlcss-arrow arrow'></i>
+            <ul class="htmlCss-sub-menu sub-menu">
+              <li><a href="pages/conta.php">CONTA</a></li>
+              <li><a href="pages/login.php">LOGIN</a></li>
+              <li><a href="pages/cadastro.php">CADASTRO</a></li>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
     
 </body>
 </html>
