68fe44fe67d9ffd95e06b45328d8b605cd1ba874
aureliointhernet2010 <aurelio2013silva@gmail.com>
2023-04-16 12:19:33 -0300

    versao 3.2

--- UVS/pages/medicos.php
+++ UVS/pages/medicos.php
@@ -13,6 +13,9 @@
 
      <!--Menu formatação-->
      <link rel="stylesheet" href="../CSS/menu.css">
+     
+     <!--Rodapé formatação-->
+     <link rel="stylesheet" href="../CSS/style.css">
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
@@ -33,9 +36,9 @@
             <a href="#">CONTA</a>
             <i class='bx bxs-chevron-down htmlcss-arrow arrow'></i>
             <ul class="htmlCss-sub-menu sub-menu">
-              <li><a href="pages/conta.php">CONTA</a></li>
-              <li><a href="pages/login.php">LOGIN</a></li>
-              <li><a href="pages/cadastro.php">CADASTRO</a></li>
+              <li><a href="../pages/conta.php">CONTA</a></li>
+              <li><a href="../pages/login.php">LOGIN</a></li>
+              <li><a href="../pages/cadastro.php">CADASTRO</a></li>
           </li>
         </ul>
       </div>
@@ -55,9 +58,50 @@
        <h1><b>ROTINA DE MÉDICOS <br> NO SISTEMA</b></h1>
        </div>
 
+       <div class="texto-informativo">
+         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br> Praesent congue faucibus erat. 
+           Cras tempus mauris dignissim ante accumsan, porta lacinia orci vestibulum.<br>
+           Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.<br>
+            Curabitur sapien metus, consequat quis condimentum vel, rhoncus ut nisi.<br> Vestibulum at nisi vehicula,
+            dictum mauris non,<br> luctus sapien. Nullam quis elit diam. Suspendisse vitae mollis enim.<br>
+            Integer tristique quam in gravida condimentum. Integer quam urna, <br>ultricies lobortis scelerisque vitae, 
+            suscipit eget massa. Proin ultricies rutrum nisl nec venenatis. .</p>
+       </div>
+        <br>
+       <div class="titulo-1">
+         <h1><b>INFORMAÇÕES</b></h1>
+       </div>
        <br>
-      
+
+       <hr>
+
+       <div class="titulo-1">
+       <h1><b>Informações sobre a rotina dos profissionais de <br> saúde de sua unidade</b></h1>
+       </div>
 </div>
   </main>
+
+  <footer>
+
+   <section class="rodape">
+     <img id="imagem2" src="../IMAGENS/instagram_logo.png" width="25%"></img>
+     <img id="imagem3" src="../IMAGENS/facebook_logo.png" width="25%"></img>
+
+     <img id="imagem1" src="../IMAGENS/logo.png" width="25%"></img>
+     <legend class="legenda">UVS+, o portal de acesso rápido e fácil! <br> Informações relacionadas à unidade<br> basica de sua localidade</legend>
+  
+   </section>
+
+   <div class="caixa">
+   <a href="pages/quem_somos.php" class="quem-somos">Quem somos?</a>
+  
+<b><p class="heal">&copy; Healtec2023</p></b>
+
+      
+     
+</div>
+
+    
+  </footer>
 </body>
 </html>
