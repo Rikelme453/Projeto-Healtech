e223ab0cb961ba3c0c9f578d3ef9014dddabde85
Cicero Rikelme Lopes Batista <cicerorikelmelopesbatista@gmail.com>
2023-04-23 11:54:47 -0300

    Versão 1.9.4
    Alterações em conta.php para edição de registros.

--- UVS/PHP/saveEdit.php
+++ UVS/PHP/saveEdit.php
@@ -3,20 +3,33 @@
        if(isset($_POST['alterar'])){
 
         $id = $_POST['id'];
-        $email = $_POST['email'];
         $nome = $_POST['nome'];
+        $email = $_POST['email'];
         $data = $_POST['data'];
         $senha =$_POST['senha'];
-         
-
+        
         $sqlInsert = "UPDATE tbubs
         SET nome='$nome',senha='$senha',email='$email',data='$data'
         WHERE id=$id";
+        
+
         $result = $conexao->query($sqlInsert);  
+        
      
+       }else if(isset($_POST['alterar2'])) {
+
+        $id = $_POST['id'];
+        $nome = $_POST['nome'];
+        $formacao = $_POST['formacao'];
+        $dias = $_POST['dias'];
+        $horario =$_POST['horarios'];
+         
+        $sqlInsert2 = "UPDATE tbmedicos
+        SET profissional='$nome',formacao='$formacao',dias='$dias',horarios='$horario'
+        WHERE Id=$id"; 
+        
+        $result2 = $conexao->query($sqlInsert2);  
+       
        }
-       else {
-        header('Location: ../pages/conta.php');
-       }
-      
+       header('Location: ../pages/conta.php');
      ?>
