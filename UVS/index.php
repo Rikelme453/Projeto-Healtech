8507654d9a880ba1716985595ba16416eb95f389
Ruivo.dev <skysystemenio@gmail.com>
2023-04-29 15:01:17 -0300

    atualizaçoes

--- UVS/index.php
+++ UVS/index.php
@@ -29,23 +29,22 @@
 <nav class="menu3">
     <div class="navbar">
       <i class='bx bx-menu'></i>
-      <img class="img" src="./IMAGENS/uvs-logo.png" alt="Logo da uvs" ></img>
-        <p id="text">Mais Vida! Mais Saúde!<p>
+      <img class="img" src="./IMAGENS/UVS +.svg" alt="Logo da uvs" ></img>
        <div class="nav-links"> 
        <ul class="links">
-          <li><a href="#">HOME</a></li>
-          <li><a href="pages/medicamentos.php">MEDICAMENTOS</a></li>
-          <li><a href="pages/medicos.php">MÉDICOS</li>
-          <li><a href="pages/noticias.php">CAMPANHAS E PÚBLICAÇÕES</li>
+          <li><a href="#">Home</a></li>
+          <li><a href="pages/medicamentos.php">Medicamentos</a></li>
+          <li><a href="pages/medicos.php">Médicos</li>
+          <li><a href="pages/noticias.php">Campanhas e Publicações  </li>
            
           <li>
-            <a href="#">CONTA</a>
+            <a href="#">Conta</a>
             <i class='bx bxs-chevron-down htmlcss-arrow arrow'></i>
             <ul class="htmlCss-sub-menu sub-menu">
-              <li><a href="pages/conta.php">CONTA</a></li>
-              <li><a href="pages/login.php">LOGIN</a></li>
-              <li><a href="pages/cadastro.php">CADASTRO</a></li>
-              <li><a href="admin_pages/administrador.php">ADMIN</a></li>
+              <li><a href="pages/conta.php">Conta</a></li>
+              <li><a href="pages/login.php">Login</a></li>
+              <li><a href="pages/cadastro.php">Cadastro</a></li>
+              <li><a href="admin_pages/administrador.php">Admin</a></li>
           </li>
         </ul>
       </div>
@@ -55,70 +54,61 @@
   <br><br>
   <main class="conteudo-principal">
   
-      <img class="index1" src="IMAGENS/unidade-virtual.png" alt="">
+      <img class="index1" src="IMAGENS/indeximage.svg" alt="" draggable="false">
       
-    <p id="text2">MEDICINA E ESTILO DE VIDA<p> 
+    <!-- <p id="text2">MEDICINA E ESTILO DE VIDA<p>  -->
       
     <hr>
+    <br>
 
         <section class="imagens">
 
           <div class="planos-img">
             <div class="plano-c">
-            <img src="IMAGENS/idosos_texto1.jpg" alt="" width="100%" height="286px">
-            <h2>Saúde da pessoa idosa</h2>
-            <p>O Estatuto do Idoso (Lei nº 10.741/2003) é uma iniciativa inovadora na 
-              garantia de direitos da pessoa idosa, 
-              fruto de forte mobilização da sociedade, e abrange as seguintes dimensões:
-                  direito à vida;
-                  à liberdade;
-                  ao respeito;
-                  à dignidade;
-                  à alimentação;
-                  à saúde;
-                  à convivência familiar;
-                  à convivência comunitária. 
-                
-                  Em 2013, comemoraram-se os 10 anos do Estatuto, com o reconhecimento de avanços significativos 
-                  no campo dos direitos, mas ainda existam lacunas de atenção por parte das políticas públicas. 
-                  Nesse mesmo ano foi lançado o Decreto Presidencial Compromisso Nacional para o Envelhecimento Ativo,
-                  coordenado pela Secretaria 
-                  dos Direitos Humanos e com a participação de doze ministérios, incluindo o Ministério da Saúde.</p>
+            <img src="IMAGENS/idososimage.svg" width="100%" draggable="false">
+            <hr>
+            <br>  
+            <p class="text1">A saúde é um dos pilares mais importantes da vida e isso é ainda mais<br>
+             evidente na terceira idade.
+               Por isso, é fundamental cuidar da saúde dos<br> idosos de forma preventiva,
+               para garantir que eles possam aproveitar <br>
+               ao máximo essa fase da vida.<br>
+               <br>
+               Aqui na nossa Unidade Virtual de Saúde, oferecemos uma ampla gama<br>de serviços de saúde para os idosos, desde consultas médicas regulares<br> até 
+               tratamentos especializados para doenças crônicas. 
+               Nossos profissionais<br> são altamente qualificados e experientes em cuidar da saúde dos idosos com todo<br> o cuidado e carinho que eles merecem.</p>
+               <img class="idoso" src="IMAGENS/idoso.jpg" width="19%" height="19%" draggable="false">
             </div>
-          
+            <br>
+            <hr>
             <div class="plano-c">
-            <img src="IMAGENS/dois-ciclismo.jpg" alt="" width="100%" height="286px">
-            <h2>Atividade para a vida</h2>
-            <p>A atividade física é simplesmente o movimento do corpo que consome energia: andar, subir 
-              escadas, jogar futebol, trabalhar no jardim ou dançar a noite toda são alguns 
-              exemplos de exercícios. 
-                A prática regular das atividades é capaz de promover inúmeras vantagens para o corpo 
-                humano: melhora a circulação sanguínea, fortalece o sistema imunológico, diminui o 
-                risco de doenças cardíacas e auxilia na manutenção ou perda de peso. 
-                A obesidade e a falta de atividade física constituem um papel importante no número de mortes ocasionadas pelo câncer, 
-                sendo classificadas como fatores de risco modificáveis. 
-                Considerada uma epidemia mundial pela Organização Mundial da Saúde, 
-                a obesidade é uma das principais responsáveis por doenças crônicas como 
-                hipertensão, dislipidemia, doenças cardiovasculares, diabetes e câncer.
-
-             </p>
+            <a class="idososbt" href="#">Aqui</a>
+            <img src="IMAGENS/fisicaimage.svg" alt="" width="100%">
+            <hr>
+            <br>
+            <p class="text2">As atividades físicas são fundamentais para manter a saúde em dia em todas as idades.<br>Elas trazem uma série de benefícios para o corpo e a mente, como aumento da disposição,<br> melhora do sono, controle do peso corporal, fortalecimento dos ossos e músculos, <br>redução do risco de doenças crônicas, como diabetes, hipertensão arterial e doenças cardiovasculares,<br> e ainda contribuem para a saúde mental, reduzindo o estresse e a ansiedade.
+<br>
+<br> 
+   Além disso, as atividades físicas podem ser uma ótima oportunidade para socialização,<br> já que muitas vezes são praticadas em grupo, o que pode aumentar o sentimento de <br>pertencimento e promover a interação social.</p>
             </div>
+            <br>
+            <br>
+            <br>
+            <hr>  
 
             <div class="plano-c">
-            <img src="IMAGENS/cerebro_humano.png.webp" alt="" width="100%" height="286px">
-            <h2>Saúde Mental</h2>
-            <p>A reabilitação psicossocial é compreendida como um conjunto de ações que buscam o fortalecimento, a inclusão e o exercício de direitos de cidadania de pacientes e familiares, mediante a criação e o desenvolvimento de iniciativas articuladas com os recursos do território nos campos do trabalho, habitação, educação, cultura, segurança e direitos humanos.
-
-            MANUTENÇÃO DA SAÚDE MENTAL
-            Praticar hábitos saudáveis e adotar um estilo de vida de qualidade, ajudam a manter a saúde mental em dia. 
-
-            Jamais se isole;
-            Consulte o médico regularmente;
-            Faça o tratamento terapêutico adequado;
-            Mantenha o físico e o intelectual ativos;
-            Pratique atividades físicas;
-            Tenha alimentação saudável;
-            Reforce os laços familiares e de amizades. </p>
+            <img src="IMAGENS/mentalImage.svg" alt="" width="100%" draggable="false">
+            <hr>
+            <br>
+            <p class="text2">A saúde mental é tão importante quanto a saúde física e deve ser cuidada com atenção e carinho. <br>Ela é essencial para o bem-estar geral, para a capacidade de lidar com desafios, <br>para manter relacionamentos saudáveis e para a produtividade e criatividade no trabalho.
+   <br>
+   <br>
+Manter uma boa saúde mental envolve o cuidado com as emoções, pensamentos e comportamentos. <br> É importante que as pessoas aprendam a reconhecer sinais de alerta,<br> como mudanças de humor, pensamentos negativos e dificuldade em realizar atividades cotidianas, <br>e busquem ajuda quando necessário.</p>
+            </div>
+            <br>
+            <br>
+            <br>
+            <hr>  
             </div>
          </div>
 
@@ -146,5 +136,22 @@
 
     
   </footer>
+  <style>
+      #btn-voltar-topo {
+        position: fixed;
+        bottom: 20px;
+        right: 20px;
+        cursor: pointer;
+        user-select: none;
+        
+      }
+      </style>
+      <img id="btn-voltar-topo" src="IMAGENS/up.svg" width="5%" height="5%" draggable="false">
+    
+    <script>
+      document.getElementById("btn-voltar-topo").addEventListener("click", function(){
+        window.scrollTo({top: 0, behavior: "smooth"});
+      });
+    </script>
 </body>
 </html>
