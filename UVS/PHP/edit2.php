b976bbf3d22c652c6a8e75a44c259ea68a31297f
aureliointhernet2010 <cicerorikelmelopesbatista@gmail.com>
2023-05-09 10:11:22 -0300

    Alterações diversas 2

--- UVS/PHP/edit2.php
+++ UVS/PHP/edit2.php
@@ -41,7 +41,7 @@
 <body>
 
   <div class="login1">
-  <img id="imagem1" src="../IMAGENS/logo.png" width="70%" ></img>
+  <img id="imagem1" src="../IMAGENS/UVS +.svg" width="70%" ></img>
      <h3>Você é importante!<br>
          Junte-se a nós!
      <h3>
