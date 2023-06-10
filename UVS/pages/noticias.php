3de81508d73c7e837b231df7d36df8dade6c6b85
Ruivo.dev <skysystemenio@gmail.com>
2023-06-09 21:02:51 -0300

    adasd

--- UVS/pages/noticias.php
+++ UVS/pages/noticias.php
@@ -124,10 +124,31 @@
             </div>
 <br>
              <!--Slides-->
-   <div class="carousel">
-    <img src="../IMAGENS/campanha1.svg">
-    <img src="../IMAGENS/campanha2.svg">
-    <img src="../IMAGENS/campanha3.svg">
+             <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="../IMAGENS/campanha1.svg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../IMAGENS/campanha2.svg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../IMAGENS/campanha3.svg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
 </div>
       <main class="mainNot">
         <div class="texto-principais-noticias">
@@ -334,39 +355,17 @@
         window.scrollTo({top: 0, behavior: "smooth"});
       });
     </script>
-    <script>
- (function() {
-      var carousel = document.querySelector('.carousel');
-      var images = carousel.getElementsByTagName('img');
-      var currentIndex = 0;
-      var interval;
-
-      function fadeOutImage(index) {
-        images[index].classList.remove('active');
-      }
-
-      function fadeInImage(index) {
-        images[index].classList.add('active');
-      }
-
-      function nextImage() {
-        fadeOutImage(currentIndex);
-        currentIndex = (currentIndex + 1) % images.length;
-        fadeInImage(currentIndex);
-      }
-
-      function startCarousel() {
-        interval = setInterval(nextImage, 4000); // Altere o intervalo de troca de imagens aqui (em milissegundos)
-      }
-
-      carousel.addEventListener('click', function() {
-        clearInterval(interval);
-        nextImage();
-        startCarousel();
-      });
-
-      startCarousel();
-    })();
-</script>
+  
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+    integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+    integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+    crossorigin="anonymous"></script>
 </body>
 </html>
